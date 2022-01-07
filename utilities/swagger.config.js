992a2c7c0c908f686ba97d2a31cd68265c9bbe00
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-07 12:11:26 +0000

    ss

--- utilities/swagger.config.js
+++ utilities/swagger.config.js
@@ -4,7 +4,7 @@ const options = {
             title: 'Hoja API',
             version: '1.0.0',
         },
-        host: 'https://hoja-portic.herokuapp.com/',
+        host: 'https://hoja-portic.herokuapp.com',
         // host: 'localhost:3306',
         basePath: '/',
         produces: [
