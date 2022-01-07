0b6d28f45b6d626d4948ca76040d621f948c1456
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-07 12:41:24 +0000

    pp

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cron = require('node-cron');
 const app = express();
 const port = process.env.PORT || 3306;
 // const host = 'localhost';
-const host = process.env.APP_URL || 'localhost';
 
 const authRoutes = require('./routes/auth.route')
 const users = require('./routes/users.route');
@@ -42,12 +41,12 @@ cron.schedule('0 0 * * 1', () => {
     scheduleController.refreshUserChallenges();
 });
 
-// app.listen(port, () => {
-//     console.log(`App App listen on PORT: ${port}/`)
-//     console.log(`App App listen on PORT: ${port}/api-docs`);
-// })
+app.listen(port, () => {
+    console.log(`App App listen on PORT: ${port}/`)
+    console.log(`App App listen on PORT: ${port}/api-docs`);
+})
 
-app.listen(port, host, () => {
-    console.log(`App listen: http://${host}:${port}/`)
-    console.log(`Swagger Docs: http://${host}:${port}/api-docs`);
-});
+// app.listen(port, host, () => {
+//     console.log(`App listen: http://${host}:${port}/`)
+//     console.log(`Swagger Docs: http://${host}:${port}/api-docs`);
+// });
