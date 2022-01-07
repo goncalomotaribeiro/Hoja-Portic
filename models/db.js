00eda29ab0cb286ec3fc5f4ef5b975fe33ab676c
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-07 11:58:56 +0000

    deploy

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
     logging: false
 });
 
