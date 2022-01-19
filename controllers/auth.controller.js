c869172579c4958ba3ea52f98c64c5a224ef6a86
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-19 15:08:11 +0000

    ss

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -55,7 +55,7 @@ exports.login = async (req, res) => {
         bcrypt.compare(req.body.password, user.password).then(function (result) {
             if (result) {
                 utilities.generateToken({ email: req.body.email }, (token) => {
-                    return res.status(200).json(token);
+                    return res.status(200).send(token);
                 })
             } else {
                 res.status(401).send('Not Authorized');
