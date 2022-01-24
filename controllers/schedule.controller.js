2578ae2d8b4b786d051238b8c112e5f5f7026d5b
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-24 01:14:50 +0000

    ss

--- controllers/schedule.controller.js
+++ controllers/schedule.controller.js
@@ -14,46 +14,54 @@ exports.refreshMets = async () => {
         let new_points;
         let badge;
 
-        if (position == 1) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 1});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 1 }});
-            new_points = badge.points + user.points
-            await User.update({ points: new_points}, { where: { id_user: user.id_user } });
-        }
-        if (position == 2) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 2});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 2 }});
-            new_points = badge.points + user.points
-            await User.update({ points: new_points}, { where: { id_user: user.id_user } });
-        }
-        if (position == 3) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 3});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 3 }});
-            new_points = badge.points + user.points
-            await User.update({ points: new_points}, { where: { id_user: user.id_user } });
-        }
-        if ((position <= (users.length * 0.05)) && (position > 3)) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 4});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 4 }});
-            new_points = badge.points + user.points
-            await User.update({ points: new_points}, { where: { id_user: user.id_user } });
-        }
-        if (position <= (users.length * 0.1) && (position > 3)) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 5});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 5 }});
-            new_points = badge.points + user.points
-            await User.update({ points: new_points}, { where: { id_user: user.id_user } });
-        }
-        if (position <= (users.length * 0.25) && (position > 3)) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 6});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 6 }});
-            new_points = badge.points + user.points
-            await User.update({ points: new_points}, { where: { id_user: user.id_user } });
-        }
-        if (position <= (users.length * 0.5) && (position > 3)) {
-            await UserBadges.create({ id_user: user.id_user, id_badge_board: 7});
-            badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 7 }});
-            new_points = badge.points + user.points
+        if(user.mets != 0){
+            if (position == 1) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 1});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 1 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+            if (position == 2) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 2});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 2 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+            if (position == 3) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 3});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 3 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+            if ((position <= (users.length * 0.05)) && (position > 3)) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 4});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 4 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+            if (position <= (users.length * 0.1) && (position > 3)) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 5});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 5 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+            if (position <= (users.length * 0.25) && (position > 3)) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 6});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 6 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+            if (position <= (users.length * 0.5) && (position > 3)) {
+                await UserBadges.create({ id_user: user.id_user, id_badge_board: 7});
+                badge = await BadgeBoard.findOne({attributes: ['points'], where: { id_badge_board: 7 }});
+                new_points = badge.points + user.points
+                await User.update({ points: new_points}, { where: { id_user: user.id_user } });
+            }
+        }else{
+            new_points = user.points - 5000;
+            if(new_points <= 0){
+                new_points = 0;
+            }
             await User.update({ points: new_points}, { where: { id_user: user.id_user } });
         }
     }
