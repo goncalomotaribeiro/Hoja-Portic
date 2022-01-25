3c39cac4121758192b9c918bc96186a415702287
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-25 00:23:43 +0000

    ss

--- controllers/loggedUser.controller.js
+++ controllers/loggedUser.controller.js
@@ -184,10 +184,11 @@ exports.updatePassword = async (req, res) => {
 exports.updateMets = async (req, res) => {
     try {
         const user = await User.findOne({ where: { email: req.user.data.email } });
+        const new_mets = user.mets + req.body.mets
         if (user === null) {
             res.status(404).json({ message: `Not found user with email=${req.user.data.email}.` });
         }
-        User.update(req.body, { where: { id_user: user.id_user } });
+        User.update({ mets: new_mets}, { where: { id_user: user.id_user } });
         res.status(200).json({ message: `User id_user = ${user.id_user} mets was updated successfully.` });
     } catch (err) {
         res.status(500).json({ message: `Error updating mets user with email=${req.user.data.email}` });
