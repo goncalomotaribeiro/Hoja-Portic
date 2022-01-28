a8efa1631f1f56cb519e349a8e2f39e51958120c
Goncalo Ribeiro <goncalomota.cr@gmail.com>
2022-01-28 15:48:32 +0000

    aa

--- controllers/loggedUser.controller.js
+++ controllers/loggedUser.controller.js
@@ -279,13 +279,12 @@ exports.updatePicture = async (req, res) => {
 exports.deleteAccount = async (req, res) => {
     try {
         const user = await User.findOne({ where: { email: req.user.data.email } });
-        if (user === null) {
-            res.status(404).json({ message: `Not found user with email=${req.user.data.email}.` });
+        if (!user) {
+            return res.status(404).json({ message: `Not found user with id_user=${req.user.data.email}.` });
+        } else {
+            await User.destroy({ where: { id_user: user.id_user } });
+            return res.status(200).json({ message: `Deleted user with id_user=${req.user.data.email}.` });
         }
-
-        User.destroy({ where: { id_user: user.id_user } });
-        return res.status(200).json({ message: `Deleted user with email=${req.user.data.email}.` });
-
     } catch (err) {
         res.status(500).json({ message: `Error delete user account with email=${req.user.data.email}` });
     }
